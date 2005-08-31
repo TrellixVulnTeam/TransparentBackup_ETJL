17bf03f585abcdf4c5422a808141fd14e9fc8950
gcrossland <none@none>
2005-08-31 00:10:15 +0000

    Corrected the Platforms type names and committed Platforms 2.0.0

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-transparentbackup.cmd -b tests.in\00\src0 -o tests.new\00\src0 -s BatchFile
-transparentbackup.cmd -d tests.new\00\src0\!fullstate.dtml -b tests.in\00\src1 -o tests.new\00\src1 -s BatchFile
-transparentbackup.cmd -b tests.in\00\src0 -o tests.new\00\src0 -s BashScript
-transparentbackup.cmd -d tests.new\00\src0\!fullstate.dtml -b tests.in\00\src1 -o tests.new\00\src1 -s BashScript
+transparentbackup.py -b tests.in\00\src0 -o tests.new\00\src0 -s BatchFile
+transparentbackup.py -d tests.new\00\src0\!fullstate.dtml -b tests.in\00\src1 -o tests.new\00\src1 -s BatchFile
+transparentbackup.py -b tests.in\00\src0 -o tests.new\00\src0 -s BashScript
+transparentbackup.py -d tests.new\00\src0\!fullstate.dtml -b tests.in\00\src1 -o tests.new\00\src1 -s BashScript
