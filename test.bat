f1044352fd6961d315fe69edd5fdec7c415a6b01
gcrossland <none@none>
2010-04-05 00:22:08 +0000

    Test source material is now tared up (to preserve file attributes)

--- test.bat
+++ test.bat
@@ -1,4 +1,17 @@
 @ECHO OFF
+
+I:\Apps\Platforms\Cygwin\bin\rm.exe -rf "tests.in/00/src0"
+mkdir tests.in\00\src0
+cd tests.in\00\src0
+I:\Apps\Platforms\Cygwin\bin\tar.exe -xf "../src0.tar"
+cd ..\..\..
+
+I:\Apps\Platforms\Cygwin\bin\rm.exe -rf "tests.in/00/src1"
+mkdir tests.in\00\src1
+cd tests.in\00\src1
+I:\Apps\Platforms\Cygwin\bin\tar.exe -xf "../src1.tar"
+cd ..\..\..
+
 mkdir tests.new\00\src0
 mkdir tests.new\00\src1
 transparentbackup.py -b tests.in\00\src0 -o tests.new\00\src0 -s BatchFile
