af766d1684cdfb40d41c06f49f37ef50e2bcb11f
gcrossland <none@none>
2012-04-07 19:55:32 +0100

    Made the tests a bit more resilient against the source tree moving

--- test.bat
+++ test.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 
+subst T: .
+cd /D T:
+
 I:\Apps\Platforms\Cygwin\bin\rm.exe -rf "tests.in/00/src0"
 mkdir tests.in\00\src0
 cd tests.in\00\src0
@@ -18,3 +21,5 @@ transparentbackup.py -b tests.in\00\src0 -o tests.new\00\src0 -s BatchFile
 transparentbackup.py -d tests.new\00\src0\!fullstate.dtml -b tests.in\00\src1 -o tests.new\00\src1 -s BatchFile
 transparentbackup.py -b tests.in\00\src0 -o tests.new\00\src0 -s BashScript
 transparentbackup.py -d tests.new\00\src0\!fullstate.dtml -b tests.in\00\src1 -o tests.new\00\src1 -s BashScript
+
+subst /D T:
