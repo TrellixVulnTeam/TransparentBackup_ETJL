e371a927e15b20419a6983fefdb336e3247eb3bd
gcrossland <none@none>
2017-04-19 16:31:31 +0100

    Added FilingPythonScript support for symlinks

--- tests.out/00/src1/!post_applydiffs.py
+++ tests.out/00/src1/!post_applydiffs.py
@@ -1,6 +1,9 @@
+import sys
 import os
 import os.path
 import shutil
+
+FS_ENCODING = sys.getfilesystemencoding() or sys.getdefaultencoding()
 
 def mkdir (name):
   p = os.path.join(*name)
@@ -11,10 +14,21 @@ def rmdir (name):
   os.rmdir(os.path.join(*name))
 
 def cp (src, dst):
-  shutil.copy2(os.path.join(*src), os.path.join(*dst))
+  s = os.path.join(*src)
+  d = os.path.join(*dst)
+  if os.path.islink(s):
+    os.symlink(os.readlink(s.encode(FS_ENCODING)), d)
+  else:
+    shutil.copy2(s, d)
 
 def mv (src, dst):
-  shutil.move(os.path.join(*src), os.path.join(*dst))
+  s = os.path.join(*src)
+  d = os.path.join(*dst)
+  if os.path.islink(s):
+    os.symlink(os.readlink(s.encode(FS_ENCODING)), d)
+    os.remove(s)
+  else:
+    shutil.move(s, d)
 
 def rm (name):
   os.remove(os.path.join(*name))
--- transparentbackup.py
+++ transparentbackup.py
@@ -691,9 +691,12 @@ endZip()
     else:
       head="""
 
+import sys
 import os
 import os.path
 import shutil
+
+FS_ENCODING = sys.getfilesystemencoding() or sys.getdefaultencoding()
 
 def mkdir (name):
   p = os.path.join(*name)
@@ -704,10 +707,21 @@ def rmdir (name):
   os.rmdir(os.path.join(*name))
 
 def cp (src, dst):
-  shutil.copy2(os.path.join(*src), os.path.join(*dst))
+  s = os.path.join(*src)
+  d = os.path.join(*dst)
+  if os.path.islink(s):
+    os.symlink(os.readlink(s.encode(FS_ENCODING)), d)
+  else:
+    shutil.copy2(s, d)
 
 def mv (src, dst):
-  shutil.move(os.path.join(*src), os.path.join(*dst))
+  s = os.path.join(*src)
+  d = os.path.join(*dst)
+  if os.path.islink(s):
+    os.symlink(os.readlink(s.encode(FS_ENCODING)), d)
+    os.remove(s)
+  else:
+    shutil.move(s, d)
 
 def rm (name):
   os.remove(os.path.join(*name))
